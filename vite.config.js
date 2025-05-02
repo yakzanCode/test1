03d8d20589fdac5e649cb78d304c5aad4090f743
Yakzan Code <yakzancode@outlook.com>
2025-05-03 00:26:49 +0200

    vite.config changed

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ['bootstrap-icons/font/bootstrap-icons.css']
-    }
+    outDir: 'dist'
   }
 })
